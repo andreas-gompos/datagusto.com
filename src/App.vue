0b791f53247d6e33e81d19585c3d4f302d0c12e4
Andreas Gompos <andreas.gompos@gmail.com>
2018-07-29 21:33:55 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <v-app>
+    <main>
+      <topic-classification-model></topic-classification-model>
+    </main>
+    </v-app>
+
+  </div>
+</template>
+
+<script>
+import TopicClassificationModel from './components/TopicClassificationModel'
+
+export default {
+  name: 'app',
+  components: {
+    TopicClassificationModel
+  }
+}
+</script>
+
+<style>
+
+</style>
