c13e0135d958b4bb417c450388189702c1a84fd8
Andreas Gompos <andreas.gompos@gmail.com>
2019-03-22 00:49:30 +0000

    add LICENSE

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app light>
-    <v-navigation-drawer
-      v-model="drawer"
-      temporary
-      app
-    >
+    <v-navigation-drawer v-model="drawer" temporary app>
       <v-toolbar>
         <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
         <v-toolbar-title class="blue--text text--darken-2" @click="drawer = !drawer">
@@ -14,11 +10,7 @@
 
       <v-divider></v-divider>
       <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :to="item.to"
-          :key="i"
-        >
+        <v-list-tile v-for="(item, i) in items" :to="item.to" :key="i">
           <v-list-tile-action>
             <v-icon v-html="item.icon" />
           </v-list-tile-action>
@@ -28,26 +20,24 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar
-      fixed
-      app
-    >
-      <v-toolbar-side-icon class="hidden-sm-and-up"@click="drawer = !drawer" />
+    <v-toolbar fixed app>
+      <v-toolbar-side-icon class="hidden-sm-and-up" @click="drawer = !drawer" />
 
       <v-toolbar-title class="blue--text text--darken-2">
-          <nuxt-link to="/" tag="span" style="cursor: pointer">{{title}}</nuxt-link>
+        <nuxt-link to="/" tag="span" style="cursor: pointer">{{title}}</nuxt-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-            <v-btn
-                color="blue darken-1"
-                 flat
-                v-for="menuItem in items"
-                :key="menuItem.title"
-                :to="menuItem.to">
-                <v-icon left>{{ menuItem.icon }}</v-icon>
-                {{menuItem.title}}
-            </v-btn>
+        <v-btn
+          color="blue darken-1"
+          flat
+          v-for="menuItem in items"
+          :key="menuItem.title"
+          :to="menuItem.to"
+        >
+          <v-icon left>{{ menuItem.icon }}</v-icon>
+          {{menuItem.title}}
+        </v-btn>
       </v-toolbar-items>
     </v-toolbar>
 
@@ -56,29 +46,26 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
+    <v-footer :fixed="fixed" app>
       <v-spacer></v-spacer>
-      <span>&copy;Datagusto 2019</span>
+      <span>&copy;Datagusto 2020</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        drawer: false,
-        fixed: false,
-        items: [
-          { icon: 'bubble_chart', title: 'Models', to: '/models' },
-          { icon: 'library_books', title: 'Blog', to: '/blog' },
-          { icon: 'info', title: 'About', to: '/about' }
-        ],
-        title: 'Datagusto'
-      }
-    }
+export default {
+  data() {
+    return {
+      drawer: false,
+      fixed: false,
+      items: [
+        { icon: "bubble_chart", title: "Models", to: "/models" },
+        { icon: "library_books", title: "Blog", to: "/blog" },
+        { icon: "info", title: "About", to: "/about" }
+      ],
+      title: "Datagusto"
+    };
   }
+};
 </script>
